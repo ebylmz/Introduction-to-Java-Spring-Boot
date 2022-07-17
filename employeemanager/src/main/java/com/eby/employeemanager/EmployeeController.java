8ac7ffd513d2827f4f6c6f4a1bfd48546e0398f2
Emirkan Burak Yılmaz <ebylmz17@gmail.com>
2022-07-17 17:40:11 +0300

    finish

--- employeemanager/src/main/java/com/eby/employeemanager/EmployeeController.java
+++ employeemanager/src/main/java/com/eby/employeemanager/EmployeeController.java
@@ -2,7 +2,8 @@ package com.eby.employeemanager;
 
 import com.eby.employeemanager.model.Employee;
 import com.eby.employeemanager.service.EmployeeService;
-import org.springframework.beans.factory.annotation.Autowired;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -10,50 +11,50 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 // @GetMapping is shortcut @RequestMapping(method = RequestMethod.GET)
-
 @RestController
 @RequestMapping("/employee")
-public class EmployeeResource {
+public class EmployeeController {
+    private final EmployeeService employeeService;
 
-    private EmployeeService employeeService;
-
+    private static final Logger logger = LoggerFactory.getLogger(EmployeeController.class);
 
     // @Autowired is needed?
-    public EmployeeResource(EmployeeService employeeService) {
+    public EmployeeController(EmployeeService employeeService) {
         this.employeeService = employeeService;
     }
 
     @GetMapping("/all")
     public ResponseEntity<List<Employee>> getAllEmployees() {
         List<Employee> employees = employeeService.findAllEmployees();
+        logger.info("Employees list fetched successfully.");
         return new ResponseEntity<>(employees, HttpStatus.OK);
     }
 
     @GetMapping("/find/{id}")
     public ResponseEntity<Employee> getEmployeeById(@PathVariable("id") Long id) {
         Employee e = employeeService.findEmployee(id);
+        logger.info("Employee: {} is fetched successfully.", id);
         return new ResponseEntity<>(e, HttpStatus.OK);
     }
 
     @PostMapping("/add")
     public ResponseEntity<Employee> addEmployee(@RequestBody Employee e) {
         Employee newEmployee = employeeService.addEmployee(e);
-        if (newEmployee == null)
-            return  new ResponseEntity<>(newEmployee, HttpStatus.BAD_REQUEST);
-        else
-            return  new ResponseEntity<>(newEmployee, HttpStatus.CREATED);
+        logger.info("Employee: {} is saved", e.getName());
+        return  new ResponseEntity<>(newEmployee, HttpStatus.CREATED);
     }
 
     @PutMapping("/update")
     public ResponseEntity<Employee> updateEmployee(@RequestBody Employee e) {
         Employee updateEmployee = employeeService.updateEmployee(e);
+        logger.info("Employee: {} is updated.", e.getName());
         return new ResponseEntity<>(updateEmployee, HttpStatus.OK);
     }
 
     @DeleteMapping("/delete/{id}")
     public ResponseEntity<?> deleteEmployee(@PathVariable("id") Long id) {
         employeeService.deleteEmployee(id);
+        logger.info("Employee: {} is deleted.", id);
         return  new ResponseEntity<>(HttpStatus.OK);
     }
-
 }
